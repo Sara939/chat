32d77e5f4afec31a887a53ccc3c7701109bb23fc
sara-likaount <saralikaunt@gmail.com>
2022-11-28 17:07:10 +0200

    n

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 
+
 const server = http.createServer((req,res)=>{
     fs.readFile('./index.html', (err,result)=> res.end(result)) // server.on('request')
 });
@@ -11,7 +12,7 @@ const io= require('socket.io')(server); //taking HTTP server and returning TCP s
 io.on('connection', (socket)=>{
     socket.on('massage', ({name,text})=>{
         console.log('emit');
-        io.emit('massage', {name,text})
+        io.emit('massage', {name,text}) //emiting to all other sokets that are connected
     })
 })
 
